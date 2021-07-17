58e16518a81778bf243f5706f41585a938351d11
jimmy <shenzui_da@hotmail.com>
2021-07-17 14:53:07 +0800

    Add：获取商品的一级分类接口

--- foodie-dev-api/src/main/java/work/jimmmy/foodie/controller/IndexController.java
+++ foodie-dev-api/src/main/java/work/jimmmy/foodie/controller/IndexController.java
@@ -1,5 +1,7 @@
 package work.jimmmy.foodie.controller;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,6 +15,7 @@ import work.jimmy.foodie.common.utils.JsonResultResponse;
 
 import java.util.List;
 
+@Api(value = "首页", tags = "首页展示相关的接口")
 @RestController
 @RequestMapping("index")
 public class IndexController {
@@ -26,6 +29,7 @@ public class IndexController {
      * 获取首页轮播图列表
      * @return list
      */
+    @ApiOperation(value = "获取轮播图列表", notes = "获取轮播图列表", httpMethod = "GET")
     @GetMapping("/carousel")
     public JsonResultResponse carousel() {
         List<Carousel> list = carouselService.queryAll(YesOrNo.YES.type);
@@ -33,10 +37,12 @@ public class IndexController {
     }
 
     /**
+     * 查询首页的商品一级类别
      * 首页分类展示需求：
      * 1. 第一次刷新主页查询大分类，渲染展示到首页
      * 2. 如果鼠标上移到大分类，则加载其子分类的内容，如果已经存在子分类，则不需要加载（懒加载）
      */
+    @ApiOperation(value = "获取商品的一级分类", notes = "获取商品分类", httpMethod = "GET")
     @GetMapping("/cats")
     public JsonResultResponse cats() {
         return JsonResultResponse.ok(categoryService.queryAllRootLevelCat());
